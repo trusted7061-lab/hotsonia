c87b4836d6cbba849d1d6491fc83fc7bf1a73cb6
mydigitalcrown <mydigitalcrown@gmail.com>
2026-04-02 18:59:43 +0530

    Add floating Call & WhatsApp buttons on all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Playfair_Display, Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import FloatingButtons from "@/components/FloatingButtons";
 
 const playfair = Playfair_Display({
   subsets: ["latin"],
@@ -207,6 +208,7 @@ export default function RootLayout({
         <Navbar />
         <main className="flex-1">{children}</main>
         <Footer />
+        <FloatingButtons />
       </body>
     </html>
   );
